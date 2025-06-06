205bc78d5da432fe1e2ea810e818e1634228d386
Boris Dev <borisdev@Ronalds-iPad.localdomain>
2025-06-06 14:15:52 -0700

    fixed bug in Dockefile

--- README.md
+++ README.md
@@ -50,87 +50,45 @@ Assumptions:
 
 ## Pre-requisites steps
 
-I prefix names with `nobs` because my company is called Nobsmed.
+`nobs` denotes my personal project's prefixing.
 
 ```console
-# create the registry
-
 az acr create --resource-group nobsmed --name nobsregistry --sku Basic
-
-# sanity check
-
 az acr show --name nobsregistry --query loginServer --output table
-Result
---------------------------
-nobsmedregistry.azurecr.io
+# Result
+# --------------------------
+# nobsmedregistry.azurecr.io
 ```
 
 ---
 
-## Ongoing launch steps
+## Deploy steps
 
 ```bash
-# Build backend and frontend docker images:
 cd backend
 docker build --tag nobs_backend --file docker/Dockerfile .
+docker buildx build --platform linux/amd64 -t nobs_backend_amd64 .
 cd ../frontend
 docker build --tag nobs_frontend --file docker/Dockerfile .
-
-# Tag the images for Azure Container Registry
-
 docker tag nobs_backend nobsregistry.azurecr.io/nobs_backend:v1
-
-# Sanity check
-
+docker tag nobs_backend_amd64 nobsregistry.azurecr.io/nobs_backend:latest
 docker images
-REPOSITORY                             TAG       IMAGE ID       CREATED        SIZE
-nobsregistry.azurecr.io/nobs_backend      v1        585a4696bedd   44 hours ago   197MB
-nobs_backend                           latest    585a4696bedd   43 hours ago   197MB
-nobs_frontend                          latest    585a4696bedd   43 hours ago   197MB
+# REPOSITORY                             TAG       IMAGE ID       CREATED        SIZE
+# nobsregistry.azurecr.io/nobs_backend      v1        585a4696bedd   44 hours ago   197MB
+# nobs_backend                           latest    585a4696bedd   43 hours ago   197MB
+# nobs_frontend                          latest    585a4696bedd   43 hours ago   197MB
 
-# Login to Azure Container Registry
-
+az login
 az acr login --name nobsregistry
-
-# Push image to Azure Container Registry
-
 docker push nobsregistry.azurecr.io/nobs_backend:v1
-
-**# Sanity check .....you see the output below**
-
-The push refers to repository [nobsregistry.azurecr.io/nobs_backend]
-5f70bf18a086: Preparing
-7d577052c02c: Preparing
-ac42807ce093: Preparing
-d54c60f73cbb: Preparing
-d559dc6e6c29: Preparing
-8f697a207321: Waiting
-
-
-# Sanity check that the image is in the registry
-
 az acr repository list --name nobsregistry
-[
-"nobs_backend",
-]
-```
-
-```console
-source azure_env
-source azure_deploy
-
-az containerapp create \
-  --name $API_NAME \
-  --resource-group $RESOURCE_GROUP \
-  --environment $ENVIRONMENT \
-  --image $ACR_NAME.azurecr.io/$API_NAME \
-  --target-port 8080 \
-  --ingress external \
-  --registry-server $ACR_NAME.azurecr.io \
-  --user-assigned "$IDENTITY_ID" \
-  --registry-identity "$IDENTITY_ID" \
-  --query properties.configuration.ingress.fqdn
-
+# [
+#   "nobs_backend",
+# ]
+az acr repository show-tags --name nobsregistry --repository nobs_backend
+# [
+#   "aws_prod"
+# ]
 az container delete --name sampleapp --resource-group nobsmed
 ```
 
--- azure-container-app.sh
+++ azure-container-app.sh
@@ -1,45 +1,61 @@
 #!/bin/bash
 
-# Variables
-RESOURCE_GROUP="myResourceGroup"
-LOCATION="eastus"
-CONTAINER_APP_ENVIRONMENT="myContainerAppEnv"
-CONTAINER_APP_NAME="myContainerApp"
-IMAGE="mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+# keep changing this.....
+#docker tag nobs_backend_amd64 nobsregistry.azurecr.io/nobs_backend:latest
+# docker push nobsregistry.azurecr.io/nobs_backend_amd64:v1
+TAG="v1"
+#TAG="latest"
 
-# Create a resource group
-az group create --name $RESOURCE_GROUP --location $LOCATION
+# these stay the same
+RESOURCE_GROUP="nobsmed"
+LOCATION="westus"
+CONTAINER_APP_ENVIRONMENT="prod"
+CONTAINER_APP_NAME="nobswebsite"
+#IMAGE_REPO="nobs_backend"
+IMAGE_REPO="nobs_backend_amd64"
+IMAGE_NAME="nobsregistry.azurecr.io/$IMAGE_REPO:$TAG"
+ACR_NAME="nobsregistry"
+IDENTITY="nobsmed-identity"
+REGISTRY_NAME="nobsregistry"
 
-# Create a Container Apps environment
-az containerapp env create \
-  --name $CONTAINER_APP_ENVIRONMENT \
-  --resource-group $RESOURCE_GROUP \
-  --location $LOCATION
+## Pre-requisites steps
+# DOC: https://learn.microsoft.com/en-us/azure/container-apps/managed-identity-image-pull?tabs=bash&pivots=bicep
+# az extension add --name containerapp --upgrade --allow-preview true
+# az identity create --name $IDENTITY --resource-group $RESOURCE_GROUP
+IDENTITY_ID=$(az identity show --name $IDENTITY --resource-group $RESOURCE_GROUP --query id --output tsv)
+echo "Managed Identity ID: $IDENTITY_ID"
 
-# Create a Container App
-az containerapp create \
-  --name $CONTAINER_APP_NAME \
-  --resource-group $RESOURCE_GROUP \
-  --environment $CONTAINER_APP_ENVIRONMENT \
-  --image $IMAGE \
-  --target-port 80 \
-  --ingress 'external'
+# Create registry if not already created
+# az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --admin-enabled true
+# az acr show --name $ACR_NAME --query loginServer --output table
+#
+# Create environment if not already created
+# az containerapp env create \
+#   --name $CONTAINER_APP_ENVIRONMENT \
+#   --resource-group $RESOURCE_GROUP \
+#   --location $LOCATION
 
+echo "List image repos in registry $ACR_NAME:"
+az acr repository list --name $ACR_NAME --output table
+echo "List tags in image repo $IMAGE_REPO in registry $ACR_NAME:"
+az acr repository show-tags --name $ACR_NAME --repository $IMAGE_REPO --output table
 
-az containerapp create \
-  --name $API_NAME \
-  --resource-group $RESOURCE_GROUP \
-  --environment $ENVIRONMENT \
-  --image $ACR_NAME.azurecr.io/$API_NAME \
-  --target-port 8080 \
-  --ingress external \
-  --registry-server $ACR_NAME.azurecr.io \
-  --user-assigned "$IDENTITY_ID" \
-  --registry-identity "$IDENTITY_ID" \
-  --query properties.configuration.ingress.fqdn
-
-
-# Output the URL of the Container App
+# az containerapp create --name $CONTAINER_APP_NAME \
+#     --resource-group $RESOURCE_GROUP \
+#     --environment $CONTAINER_APP_ENVIRONMENT \
+#     --user-assigned $IDENTITY_ID \
+#     --registry-identity $IDENTITY_ID \
+#     --registry-server "${REGISTRY_NAME}.azurecr.io" \
+#     --image "${IMAGE_NAME}" \
+# #    --dns-name-label  "XXXX" \
+#
+# # To access container app, you need to enable ingress over https
+# az containerapp ingress enable -n nobswebsite -g nobsmed --type external --target-port 80 --transport auto
+az containerapp ingress enable -n $CONTAINER_APP_NAME -g $RESOURCE_GROUP --type external --allow-insecure --target-port 80 --transport auto
+#
+#
 echo "Container App URL:"
+az containerapp show --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --query "{FQDN:fqdn, IP:ipAddress.ip}" --output table
 az containerapp show --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --query "properties.configuration.ingress.fqdn" -o tsv
 
+# az container show --resource-group $RESOURCE_GROUP --name $CONTAINER_APP_NAME --query " {IP:ipAddress.ip, FQDN:ipAddress.fqdn} " --output table
