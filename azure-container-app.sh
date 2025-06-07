772aaa5c10f39911df6aa8605b04ef938921b456
Boris Dev <borisdev@Boriss-MacBook-Air.local>
2025-06-07 14:05:23 -0700

    azure search works

--- azure-container-app.sh
+++ azure-container-app.sh
@@ -18,6 +18,7 @@ IMAGE_NAME="nobsregistry.azurecr.io/$IMAGE_REPO:$TAG"
 ACR_NAME="nobsregistry"
 IDENTITY="nobsmed-identity"
 REGISTRY_NAME="nobsregistry"
+SEARCH_SERVICE_NAME="nobssearch"
 
 ## Pre-requisites steps
 # DOC: https://learn.microsoft.com/en-us/azure/container-apps/managed-identity-image-pull?tabs=bash&pivots=bicep
@@ -25,6 +26,37 @@ REGISTRY_NAME="nobsregistry"
 # az identity create --name $IDENTITY --resource-group $RESOURCE_GROUP
 IDENTITY_ID=$(az identity show --name $IDENTITY --resource-group $RESOURCE_GROUP --query id --output tsv)
 echo "Managed Identity ID: $IDENTITY_ID"
+
+#az search service delete --name $SEARCH_SERVICE_NAME --resource-group $RESOURCE_GROUP
+
+# Create Azure Search Service Resource if not already created
+# az search service create --name $SEARCH_SERVICE_NAME --resource-group $RESOURCE_GROUP --sku free --location $LOCATION --output table
+# az search service create \
+#     --name $SEARCH_SERVICE_NAME \
+#     --resource-group $RESOURCE_GROUP \
+#     --sku Standard \
+#     --partition-count 1 \
+#     --replica-count 1 \
+#     --identity-type SystemAssigned
+#
+echo ""
+echo "List all search services in subscription:"
+az resource list --resource-type Microsoft.Search/searchServices --output table
+echo ""
+echo "List of Search Services in Resource Group $RESOURCE_GROUP:"
+az search service list --resource-group $RESOURCE_GROUP --output table
+echo ""
+echo "Search Service Hostname:"
+az search service show --name $SEARCH_SERVICE_NAME --resource-group $RESOURCE_GROUP --query "hostName" --output table
+echo ""
+echo "Search Service API Key:"
+az search admin-key show --service-name $SEARCH_SERVICE_NAME --resource-group $RESOURCE_GROUP --query "primaryKey" --output tsv
+echo ""
+# https://nobssearch.search.windows.net
+#Search Service API Key:
+
+
+
 
 # Create registry if not already created
 # az acr create --resource-group $RESOURCE_GROUP --name $ACR_NAME --sku Basic --admin-enabled true
@@ -36,30 +68,30 @@ echo "Managed Identity ID: $IDENTITY_ID"
 #   --resource-group $RESOURCE_GROUP \
 #   --location $LOCATION
 
-echo "List image repos in registry $ACR_NAME:"
-az acr repository list --name $ACR_NAME --output table
-echo "List tags in image repo $IMAGE_REPO in registry $ACR_NAME:"
-az acr repository show-tags --name $ACR_NAME --repository $IMAGE_REPO --output table
+# echo "List image repos in registry $ACR_NAME:"
+# az acr repository list --name $ACR_NAME --output table
+# echo "List tags in image repo $IMAGE_REPO in registry $ACR_NAME:"
+# az acr repository show-tags --name $ACR_NAME --repository $IMAGE_REPO --output table
 
 
 ## CANT SEND TO LOGFIRE FROM CONTAINER APP --- change some egress settings
 
-az containerapp create --name $CONTAINER_APP_NAME \
-    --resource-group $RESOURCE_GROUP \
-    --environment $CONTAINER_APP_ENVIRONMENT \
-    --user-assigned $IDENTITY_ID \
-    --registry-identity $IDENTITY_ID \
-    --registry-server "${REGISTRY_NAME}.azurecr.io" \
-    --image "${IMAGE_NAME}" \
-    --env-vars LOGFIRE_TOKEN=$LOGFIRE_TOKEN AZURE_OPENAI_API_KEY=$AZURE_OPENAI_API_KEY WEST_API_KEY=$WEST_API_KEY EASTUS2_API_KEY=$EASTUS2_API_KEY API_KEY=$API_KEY OPENSEARCH_HOST=nlp.nobsmed-api.com LOGFIRE_SEND_TO_LOGFIRE=0 LOGFIRE_ENVIRONMENT=PROD WEB_APP_ENV=PROD
+# az containerapp create --name $CONTAINER_APP_NAME \
+#     --resource-group $RESOURCE_GROUP \
+#     --environment $CONTAINER_APP_ENVIRONMENT \
+#     --user-assigned $IDENTITY_ID \
+#     --registry-identity $IDENTITY_ID \
+#     --registry-server "${REGISTRY_NAME}.azurecr.io" \
+#     --image "${IMAGE_NAME}" \
+#     --env-vars LOGFIRE_TOKEN=$LOGFIRE_TOKEN AZURE_OPENAI_API_KEY=$AZURE_OPENAI_API_KEY WEST_API_KEY=$WEST_API_KEY EASTUS2_API_KEY=$EASTUS2_API_KEY API_KEY=$API_KEY OPENSEARCH_HOST=nlp.nobsmed-api.com LOGFIRE_SEND_TO_LOGFIRE=0 LOGFIRE_ENVIRONMENT=PROD WEB_APP_ENV=PROD
 
-# To access container app, you need to enable ingress over https
-az containerapp ingress enable -n nobswebsite -g nobsmed --type external --target-port 80 --transport auto
-az containerapp ingress enable -n $CONTAINER_APP_NAME -g $RESOURCE_GROUP --type external --allow-insecure --target-port 80 --transport auto
+# # To access container app, you need to enable ingress over https
+# az containerapp ingress enable -n nobswebsite -g nobsmed --type external --target-port 80 --transport auto
+# az containerapp ingress enable -n $CONTAINER_APP_NAME -g $RESOURCE_GROUP --type external --allow-insecure --target-port 80 --transport auto
+# #
+# #
+# echo "Container App URL:"
+# az containerapp show --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --query "{FQDN:fqdn, IP:ipAddress.ip}" --output table
+# az containerapp show --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --query "properties.configuration.ingress.fqdn" -o tsv
 #
-#
-echo "Container App URL:"
-az containerapp show --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --query "{FQDN:fqdn, IP:ipAddress.ip}" --output table
-az containerapp show --name $CONTAINER_APP_NAME --resource-group $RESOURCE_GROUP --query "properties.configuration.ingress.fqdn" -o tsv
-
-# az container show --resource-group $RESOURCE_GROUP --name $CONTAINER_APP_NAME --query " {IP:ipAddress.ip, FQDN:ipAddress.fqdn} " --output table
+# # az container show --resource-group $RESOURCE_GROUP --name $CONTAINER_APP_NAME --query " {IP:ipAddress.ip, FQDN:ipAddress.fqdn} " --output table
---
+++ backend/website/azure_search_client.py
@@ -0,0 +1,184 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""
+https://learn.microsoft.com/en-us/python/api/overview/azure/search-documents-readme?view=azure-python#querying
+https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/search/azure-search-documents/samples/sample_vector_search.py
+
+
+FILE: sample_vector_search.py
+DESCRIPTION:
+    This sample demonstrates how to get search results from a basic search text
+    from an Azure Search index.
+USAGE:
+    python sample_vector_search.py
+
+    Set the environment variables with your own values before running the sample:
+    1) AZURE_SEARCH_SERVICE_ENDPOINT - the endpoint of your Azure Cognitive Search service
+    2) AZURE_SEARCH_INDEX_NAME - the name of your search index (e.g. "hotels-sample-index")
+    3) AZURE_SEARCH_API_KEY - your search API key
+"""
+
+import os
+
+from azure.core.credentials import AzureKeyCredential
+from azure.search.documents import SearchClient
+from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.models import VectorizedQuery
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".secret")  # Load environment variables from .secret file
+service_endpoint = os.environ["AZURE_SEARCH_SERVICE_ENDPOINT"]
+index_name = os.environ["AZURE_SEARCH_INDEX_NAME"]
+key = os.environ["AZURE_SEARCH_API_KEY"]
+
+
+def get_embeddings(text: str):
+    # There are a few ways to get embeddings. This is just one example.
+    import openai
+
+    open_ai_endpoint = os.getenv("OpenAIEndpoint")
+    open_ai_key = os.getenv("OpenAIKey")
+
+    client = openai.AzureOpenAI(
+        azure_endpoint=open_ai_endpoint,
+        api_key=open_ai_key,
+        api_version="2023-09-01-preview",
+    )
+    embedding = client.embeddings.create(input=[text], model="text-embedding-ada-002")
+    return embedding.data[0].embedding
+
+
+def get_hotel_index(name: str):
+    from azure.search.documents.indexes.models import (
+        HnswAlgorithmConfiguration, SearchableField, SearchField,
+        SearchFieldDataType, SearchIndex, SimpleField, VectorSearch,
+        VectorSearchProfile)
+
+    fields = [
+        SimpleField(name="hotelId", type=SearchFieldDataType.String, key=True),
+        SearchableField(
+            name="hotelName",
+            type=SearchFieldDataType.String,
+            sortable=True,
+            filterable=True,
+        ),
+        SearchableField(name="description", type=SearchFieldDataType.String),
+        # SearchField(
+        #     name="descriptionVector",
+        #     type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+        #     searchable=True,
+        #     vector_search_dimensions=1536,
+        #     vector_search_profile_name="my-vector-config",
+        # ),
+        SearchableField(
+            name="category",
+            type=SearchFieldDataType.String,
+            sortable=True,
+            filterable=True,
+            facetable=True,
+        ),
+    ]
+    vector_search = VectorSearch(
+        profiles=[
+            VectorSearchProfile(
+                name="my-vector-config",
+                algorithm_configuration_name="my-algorithms-config",
+            )
+        ],
+        algorithms=[HnswAlgorithmConfiguration(name="my-algorithms-config")],
+    )
+    return SearchIndex(name=name, fields=fields, vector_search=vector_search)
+
+
+def get_hotel_documents():
+    docs = [
+        {
+            "hotelId": "1",
+            "hotelName": "Fancy Stay",
+            "description": "Best hotel in town if you like luxury hotels.",
+            # "descriptionVector": get_embeddings(
+            #     "Best hotel in town if you like luxury hotels."
+            # ),
+            "category": "Luxury",
+        },
+        {
+            "hotelId": "2",
+            "hotelName": "Roach Motel",
+            "description": "Cheapest hotel in town. Infact, a motel.",
+            # "descriptionVector": get_embeddings(
+            #     "Cheapest hotel in town. Infact, a motel."
+            # ),
+            "category": "Budget",
+        },
+        {
+            "hotelId": "3",
+            "hotelName": "EconoStay",
+            "description": "Very popular hotel in town.",
+            # "descriptionVector": get_embeddings("Very popular hotel in town."),
+            "category": "Budget",
+        },
+        {
+            "hotelId": "4",
+            "hotelName": "Modern Stay",
+            "description": "Modern architecture, very polite staff and very clean. Also very affordable.",
+            # "descriptionVector": get_embeddings(
+            #     "Modern architecture, very polite staff and very clean. Also very affordable."
+            # ),
+            "category": "Luxury",
+        },
+        {
+            "hotelId": "5",
+            "hotelName": "Secret Point",
+            "description": "One of the best hotel in town. The hotel is ideally located on the main commercial artery of the city in the heart of New York.",
+            # "descriptionVector": get_embeddings(
+            #     "One of the best hotel in town. The hotel is ideally located on the main commercial artery of the city in the heart of New York."
+            # ),
+            "category": "Boutique",
+        },
+    ]
+    return docs
+
+
+if __name__ == "__main__":
+    credential = AzureKeyCredential(key)
+    index_client = SearchIndexClient(service_endpoint, credential)
+    index = get_hotel_index(index_name)
+    try:
+        index_client.create_index(index)
+    except Exception as e:
+        print(f"Index {index_name} already exists. Error: {e}")
+        pass
+    client = SearchClient(service_endpoint, index_name, credential)
+    hotel_docs = get_hotel_documents()
+    client.upload_documents(documents=hotel_docs)
+
+    query = "Top hotels in town"
+    query = "boris johnson"
+    query = "town x"
+    print(f"\nSearch results for hotels using query: {query}")
+    print("=====================================================")
+    search_client = SearchClient(service_endpoint, index_name, AzureKeyCredential(key))
+    results = search_client.search(
+        search_text=query,
+        select=["hotelId", "hotelName"],
+    )
+
+    for result in results:
+        print(result)
+
+    filter = "category eq 'Luxury'"
+    print(f"\nSearch results for hotels using filter: {filter}")
+    print("=====================================================")
+    results = search_client.search(
+        search_text="",
+        filter="category eq 'Luxury'",
+        select=["hotelId", "hotelName"],
+    )
+
+    for result in results:
+        print(result)
